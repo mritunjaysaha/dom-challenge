d47fef4d96940e6740df3821eaf5759c302fb0de
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-07-26 21:03:27 +0530

    completed challenge 3

--- Challenge 3/app.js
+++ Challenge 3/app.js
@@ -1,12 +1,13 @@
-function Board(el, rows = 8, cols = 8) {
+function Board(el, rows = 8, cols = 8, color = "red") {
     this.el = document.querySelector(el);
     console.log(this.el);
     this.rows = rows;
     this.cols = cols;
-    this.color = "red";
+    this.color = color;
     this.activeCell = "";
 
     this.generateBoard();
+    this.bindEvents();
 }
 
 Board.prototype.generateBoard = function () {
@@ -29,29 +30,66 @@ Board.prototype.generateBoard = function () {
     this.el.appendChild(fragment);
 };
 
-// Board.prototype.bindEvents = function () {
-//     this.el.addEventListener("click", (e) => {
-//         this.activeCell && this.diagonals(this.activeCell, null);
+Board.prototype.bindEvents = function () {
+    this.el.addEventListener("click", (e) => {
+        this.activeCell && this.diagonals(this.activeCell, null);
 
-//         var cell = e.target.dataset["cell"];
-//         this.activeCell = cell;
-//         cell && this.diagonals(cell, this.color);
+        var cell = e.target.dataset["cell"];
+        this.activeCell = cell;
+        this.diagonals(cell, this.color);
 
-//         e.stopPropagation();
-//     });
+        e.stopPropagation();
+    });
 
-//     document.addEventListener("click", (e) => {
-//         this.activeCell && this.diagnols(this.activeCell, null);
-//     });
-// };
+    document.addEventListener("click", (e) => {
+        this.activeCell && this.diagnols(this.activeCell, null);
+    });
+};
 
-// Board.prototype.fill = function (cell, color) {
-//     document.querySelector(`div[data-cell='${cell}']`).style.background = color;
-// };
+Board.prototype.fill = function (cell, color) {
+    document.querySelector(`div[data-cell='${cell}']`).style.background = color;
+};
 
-// Board.prototype.diagnols = function (activeCell, color) {
-//     const [left, right] = activeCell.split(":");
+Board.prototype.diagonals = function (cell, color) {
+    console.log(cell);
 
-// }
+    let [row, col] = cell.split(":");
+    row = Number.parseInt(row);
+    col = Number.parseInt(col);
+
+    this.fill(cell, color);
+
+    // 1:2 active cell
+
+    // top-left
+    // 0:1
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+        console.log(i + ":" + j);
+        this.fill(i + ":" + j, color);
+    }
+    // bottom-left
+    // 2:1
+    // 3:0
+    for (let i = row + 1, j = col - 1; i < 8 && j >= 0; i++, j--) {
+        console.log(i + ":" + j);
+        this.fill(i + ":" + j, color);
+    }
+    // top-right
+    // 0:3
+    for (let i = row - 1, j = col + 1; i >= 0 && j < 8; i--, j++) {
+        console.log(i + ":" + j);
+        this.fill(i + ":" + j, color);
+    }
+    // bottom-right
+    // 2:3
+    // 3:4
+    // 4:5
+    // 5:6
+    // 6:7
+    for (let i = row + 1, j = col + 1; i < 8 && j < 8; i++, j++) {
+        console.log(i + ":" + j);
+        this.fill(i + ":" + j, color);
+    }
+};
 
 new Board("#board");
