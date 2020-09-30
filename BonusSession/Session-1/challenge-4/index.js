a3ddf855ddeefe25e0c35830b1170a195695cc14
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-09-30 21:54:54 +0530

    completed challenge-4

--- BonusSession/Session-1/challenge-4/index.js
+++ BonusSession/Session-1/challenge-4/index.js
@@ -1,6 +1,20 @@
 class Loader {
-    constructor(el, increment) {
-        this.el = document.querySelector(el);
+    constructor(loader, increment) {
+        this.loader = document.querySelector(loader);
         this.increment = document.querySelector(increment);
+
+        this.loaderFilled = 0;
+
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        this.increment.addEventListener("click", (e) => {
+            const cell = e.target.dataset["inc"];
+
+            this.loaderFilled += Number(cell);
+
+            this.loader.style.width = `${this.loaderFilled}%`;
+        });
     }
 }
