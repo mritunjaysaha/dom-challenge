eee98ad0bba621e458038ac2520f0fbc73b9f7ab
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-09-11 12:51:03 +0530

    challenge 6 completed

--- Challenge 6/index.js
+++ Challenge 6/index.js
@@ -1,18 +1,70 @@
 class Board {
-    constructor(el, startEl, cols = 5) {
+    constructor(el, startEl, scoreEl, cols = 5) {
         this.el = document.querySelector(el);
         this.startEl = document.querySelector(startEl);
+        this.scoreEl = document.querySelector(scoreEl);
 
         this.cols = cols;
         this.score = 0;
-
-        this.error = false;
+        this.blinkCellsArr = [];
+        this.userClicks = -1;
+        this.numberOfTimesClicked = 0;
 
         this.init();
         this.bindEvents();
     }
 
     init() {
+        this.scoreEl.innerText = this.score;
+
+        this.generateBoard();
+    }
+
+    bindEvents() {
+        this.el.addEventListener("click", (e) => {
+            const currentCell = Number.parseInt(e.target.dataset["cell"]);
+
+            this.userClicks++;
+            this.numberOfTimesClicked++;
+
+            console.log(
+                "user clicks",
+                this.userClicks,
+                this.blinkCellsArr[this.userClicks]
+            );
+            if (this.blinkCellsArr[this.userClicks] === currentCell) {
+                console.log("true");
+
+                this.updateScore(currentCell);
+            } else {
+                this.el.classList.add("shake");
+
+                this.score = 0;
+                this.blinkCellsArr = [];
+                this.userClicks = -1;
+                this.numberOfTimesClicked = 0;
+
+                const wrongCell = document.querySelector(
+                    `div[data-cell='${currentCell}']`
+                );
+
+                wrongCell.style.background = "red";
+
+                setTimeout(() => {
+                    wrongCell.style.background = "white";
+
+                    this.el.classList.remove("shake");
+                }, 800);
+            }
+        });
+
+        this.startEl.addEventListener("click", () => {
+            console.log("clicked");
+            this.startBlink();
+        });
+    }
+
+    generateBoard() {
         const fragment = document.createDocumentFragment();
 
         for (let i = 0; i < this.cols; i++) {
@@ -26,12 +78,25 @@ class Board {
         this.el.appendChild(fragment);
     }
 
-    bindEvents() {
-        this.el.addEventListener("click", (e) => {});
-        this.startEl.addEventListener("click", () => {
-            console.log("clicked");
+    updateScore(currentCell) {
+        console.log("update score");
+
+        this.blinkCellsArr[this.userClicks] = "";
+
+        console.log(this.blinkCellsArr);
+        console.log("no clicked", this.numberOfTimesClicked);
+
+        if (this.blinkCellsArr.length === this.numberOfTimesClicked) {
+            console.log("start blink");
+            this.userClicks = -1;
+            this.blinkCellsArr = [];
+            this.numberOfTimesClicked = 0;
+
+            this.score++;
+            this.scoreEl.innerText = this.score;
+
             this.startBlink();
-        });
+        }
     }
 
     startBlink() {
@@ -50,8 +115,18 @@ class Board {
 
     selectRandomCell(max = this.cols) {
         const cell = Number.parseInt(Math.random() * max);
-        console.log("cell", cell);
+        this.blinkCellsArr.push(cell);
+
+        console.log(this.blinkCellsArr);
+
+        const blinkCell = document.querySelector(`div[data-cell='${cell}']`);
+
+        blinkCell.style.background = "blue";
+
+        setTimeout(() => {
+            blinkCell.style.background = "white";
+        }, 800);
     }
 }
 
-new Board("#board", "#start");
+new Board("#board", "#start", "#score");
